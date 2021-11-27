7178f2c2a21ec33cfc96fc80e53501999d37ef50
profusio <47553950+profusio@users.noreply.github.com>
2021-11-27 20:44:21 +0100

    working muktiple fetch

--- frontend/src/components/MetamDatas/MetamDatas.js
+++ frontend/src/components/MetamDatas/MetamDatas.js
@@ -7,11 +7,16 @@ import classNames from 'classnames';
 export default function MetamDatas(props) {
     //var Newprice = Math.round(props.data[0].price * 100) / 100;
 
+
+   console.log(props.dataBalance);
+   console.log(props.dataGas);
+   console.log(props.dataTime);
+
     return( 
 
             <Card className ="h100" >
                     <ListGroup variant="flush">
-                    
+
 
                     </ListGroup>
             </Card>
--- frontend/src/containers/Metamask/Metamask.js
+++ frontend/src/containers/Metamask/Metamask.js
@@ -1,76 +1,74 @@
 import React, { useState, useEffect } from 'react';
+
 import axios from 'axios';
+
+
 
 import MetamDatas from '../../components/MetamDatas/MetamDatas';
 
 
+
+
 export default function Metamask() {
-   
-    const [data, setData] = useState([]); //Portefeuille à notre adresse
-    //const [dataGas, setGas] = useState([]);//Frais transaction suggérés sur ETH en live (en GWEI)
-    //const [dataTime, setTime] = useState([]);//Temps de transaction moyen estimé sur ETH en live (en secondes)
+
+    const [data, setData] = useState([]);
+    const [dataGas, setDataGas]=useState([]);
+    const[dataTime, setDataTime]=useState([]);
 
 
-    useEffect(() => {
-       
-        
-        //fonctionnel : Wallet Balance
-        const fetchData = async () => {
-        
-            const result = await axios(
-                'https://api.etherscan.io/api?module=account&action=balance&address=0x6bC26E853086CF244849D3780eC5C4BF80106410&tag=latest&apikey=35BFWIG7F816FH2RSHCA9YC4UNHM1EXRF4', //results in wei
-                );
+  const fetchData = () => {
 
-            setData(result.data);
-             console.log(result);
-        };
-       
+    const ETHBalance = `https://api.etherscan.io/api?module=account&action=balance&address=0x6bC26E853086CF244849D3780eC5C4BF80106410&tag=latest&apikey=35BFWIG7F816FH2RSHCA9YC4UNHM1EXRF4`;
+    const ETHGas = 'https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=J4YRQYN2TA4RFTSFU9KC2B8YA3ZW9BW888';
+    const ETHDataTime =  `https://api.etherscan.io/api?module=gastracker&action=gasestimate&gasprice=2000000000&apikey=HBE5A1TV3KITG8W1FNZSSHFVI8DE3FM8XQ`;
+    
+    const getETHBalance = axios.get(ETHBalance);
+    const getETHGas = axios.get(ETHGas);
+    const getETHDataTime = axios.get(ETHDataTime);
 
-        const fetchData2 = async () => {
-        
-            const resultGas = await axios(
-                `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=J4YRQYN2TA4RFTSFU9KC2B8YA3ZW9BW888`, 
-                //"SafeGasPrice","ProposeGasPrice","FastGasPrice", (en gwei 10^-9)
-            );
+    axios.all([getETHBalance,getETHGas,getETHDataTime]).then(
+        axios.spread((...allData) => {
 
-            setData(resultGas.data);
-            console.log(resultGas);
-        };
 
-        const fetchData3 = async () => {
-        
-            const resultTime = await axios(
-                `https://api.etherscan.io/api?module=gastracker&action=gasestimate&gasprice=2000000000&apikey=HBE5A1TV3KITG8W1FNZSSHFVI8DE3FM8XQ`,// "result" (en secondes)
-            );
+            const allDataETHBalance = allData[0];
+            const allDataETHGas = allData[1];
+            const allDataETHDataTime = allData[2];
 
-           setData(resultTime.data);
-           console.log(resultTime);
-        };
-        
-        fetchData();
-        fetchData2();
-        fetchData3();
+            //console.log(allDataETHBalance);
+            setData(allDataETHBalance.data);
+           
+           // console.log(allDataETHGas);
+            setDataGas(allDataETHGas.data);
+           
+           // console.log(allDataETHDataTime);
+            setDataTime(allDataETHDataTime.data);
 
-       /* console.log(data);
-        console.log(data2);
-        console.log(data3);
 
-       /*while(data.length===0 || data2.length===0 || data3.length===0 ){
+        })
+    )
 
-        console.log("LOADIUNG");
 
-        fetchData();
-        fetchData2();
-        fetchData3();
+  }
 
-        }*/
+  useEffect(() => {
 
-    },[])
+fetchData();     
 
+    
+},[])
+
+    if (data.length===0 || dataGas.length===0 || dataTime.length===0)  //Si tableaux vide cad api pas encore fetch
+
+        return <div>Loading...</div>;
 
     return (
+
         <div>
 
+            <MetamDatas dataBalance={data} dataGas={dataGas}  dataTime={dataTime}/>
+
         </div>
+
     )
+
 }

1a1dd828337d8918de17b56f2159d52f4311ea29
profusio <47553950+profusio@users.noreply.github.com>
2021-11-27 17:26:59 +0100

    3fetch OK

--- frontend/src/containers/Metamask/Metamask.js
+++ frontend/src/containers/Metamask/Metamask.js
@@ -6,69 +6,67 @@ import MetamDatas from '../../components/MetamDatas/MetamDatas';
 
 export default function Metamask() {
    
-    //const [data, setData] = useState([]); //Portefeuille à notre adresse
+    const [data, setData] = useState([]); //Portefeuille à notre adresse
     //const [dataGas, setGas] = useState([]);//Frais transaction suggérés sur ETH en live (en GWEI)
     //const [dataTime, setTime] = useState([]);//Temps de transaction moyen estimé sur ETH en live (en secondes)
 
-    const [data, setData] = useState([]);//Temps de transaction moyen estimé sur ETH en live (en secondes)
 
     useEffect(() => {
        
-        //fonctionnel
+        
+        //fonctionnel : Wallet Balance
         const fetchData = async () => {
         
             const result = await axios(
-                'https://api.etherscan.io/api?module=account&action=balance&address=0x6bC26E853086CF244849D3780eC5C4BF80106410&tag=latest&apikey=GCJDGGPK7KCWABIFNZ634YT3F8NMJX8TZW', //results in wei
+                'https://api.etherscan.io/api?module=account&action=balance&address=0x6bC26E853086CF244849D3780eC5C4BF80106410&tag=latest&apikey=35BFWIG7F816FH2RSHCA9YC4UNHM1EXRF4', //results in wei
                 );
 
             setData(result.data);
-            console.log(data);
+             console.log(result);
         };
        
-        /* const fetchData1 = async () => {
-        
-            const result = await axios(
-                'https://api.etherscan.io/api?module=account&action=balance&address=0x6bC26E853086CF244849D3780eC5C4BF80106410&tag=latest&apikey=GCJDGGPK7KCWABIFNZ634YT3F8NMJX8TZW', //results in wei
-                ); //"result" est en wei (10^-18eth)
 
-            setData(result.data);
-            console.log(data);
-        };
-
-       /* const fetchData2 = async () => {
+        const fetchData2 = async () => {
         
             const resultGas = await axios(
-                `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=GCJDGGPK7KCWABIFNZ634YT3F8NMJX8TZW`, 
+                `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=J4YRQYN2TA4RFTSFU9KC2B8YA3ZW9BW888`, 
                 //"SafeGasPrice","ProposeGasPrice","FastGasPrice", (en gwei 10^-9)
             );
 
-            setGas(resultGas.dataGas);
-            console.log(dataGas);
-        };*/
+            setData(resultGas.data);
+            console.log(resultGas);
+        };
 
-       /* const fetchData3 = async () => {
+        const fetchData3 = async () => {
         
             const resultTime = await axios(
-                `https://api.etherscan.io/api?module=gastracker&action=gasestimate&gasprice=2000000000&apikey=GCJDGGPK7KCWABIFNZ634YT3F8NMJX8TZW`,// "result" (en secondes)
+                `https://api.etherscan.io/api?module=gastracker&action=gasestimate&gasprice=2000000000&apikey=HBE5A1TV3KITG8W1FNZSSHFVI8DE3FM8XQ`,// "result" (en secondes)
             );
 
-            setTime(resultTime.dataTime);
-            console.log(dataTime);
-        };*/
-
+           setData(resultTime.data);
+           console.log(resultTime);
+        };
         
         fetchData();
-        //fetchData2();
-        //fetchData3();
+        fetchData2();
+        fetchData3();
+
+       /* console.log(data);
+        console.log(data2);
+        console.log(data3);
+
+       /*while(data.length===0 || data2.length===0 || data3.length===0 ){
+
+        console.log("LOADIUNG");
+
+        fetchData();
+        fetchData2();
+        fetchData3();
+
+        }*/
 
     },[])
 
-   /* if (data.status===0) //Si tableau vide cad api pas encore fetch
-        return <div>Loading...</div>;
-    /*if (dataGas.length===0) //Si tableau vide cad api pas encore fetch
-        return <div>Loading...</div>;*/
-    /*if (dataTime.length===0) //Si tableau vide cad api pas encore fetch
-        return <div>Loading...</div>;  */
 
     return (
         <div>
